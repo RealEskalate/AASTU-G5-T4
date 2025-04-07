1f2a160685c7555ef7ea9cf639631cc4f2af50b9
Bz14 <bezueyerusalem@gmail.com>
2025-04-07 19:23:24 +0300

    feature: dockerization and air setup

--- Backend/pkg/database/database.go
+++ Backend/pkg/database/database.go
@@ -1,18 +1,26 @@
 package database
 
 import (
+<<<<<<< Updated upstream
+=======
+	"A2SVHUB/pkg/config"
+>>>>>>> Stashed changes
 	"fmt"
 	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+<<<<<<< Updated upstream
 
 	"A2SVHUB/pkg/config"
+=======
+>>>>>>> Stashed changes
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
+<<<<<<< Updated upstream
 	// Load configuration
 	cfg := config.LoadConfig()
 
@@ -28,4 +36,16 @@ func ConnectDB() {
 	}
 
 	fmt.Println("Connected to database successfully")
+=======
+   cfg := config.LoadConfig()
+   dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort, cfg.SSLMode)
+   var err error
+   DB, err = gorm.Open(postgres.Open(dsn))
+   if err != nil {
+       log.Fatal("Failed to connect to database")
+   }
+   fmt.Println("Connected to database successfully")
+>>>>>>> Stashed changes
 }
+
+
