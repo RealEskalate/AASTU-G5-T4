d3fe8734e6422479b6732ec7271edd08b951ade9
ikramawol <ikruawol.14@gmail.com>
2025-04-09 19:24:22 +0300

    group

--- Backend/api/controllers/group.go
+++ Backend/api/controllers/group.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"AASTU-G5-T4/Backend/internal/domain/group"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,4 +10,9 @@ func Group(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"group": "A2SV",
 	})
+}
+
+func createGroup(c *gin.Context) {
+	var group group.Group
+
 }
