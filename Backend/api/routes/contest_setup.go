afb38b354e6c5ec4897a2c845d111427c29669aa
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:10:25 +0300

    Wrap contest endpoints with auth middleware

--- Backend/api/routes/contest_setup.go
+++ Backend/api/routes/contest_setup.go
@@ -5,12 +5,14 @@ import (
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
 	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupContestGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB) {
+func SetupContestGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
 
 	contestRepo := repositories.NewContestRepository(*db)
 	cacheRepo := repositories.NewCacheRepository()
@@ -21,6 +23,10 @@ func SetupContestGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB) {
 	userUseCase := usecases.NewUserUseCase(userRepo)
 
 	contestController := controllers.NewContestController(contestUseCase, userUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
 
 	api.GET("", contestController.GetAllContests)
 	api.POST("", contestController.AddContest)
