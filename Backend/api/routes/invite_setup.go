b7a89acf01c7257eae773f3a6353c6223d521e2a
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:11:38 +0300

    Wrap invite endpoints with auth middleware

--- Backend/api/routes/invite_setup.go
+++ Backend/api/routes/invite_setup.go
@@ -5,18 +5,22 @@ import (
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
 	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
 	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupInviteGroup(inviteRoute *gin.RouterGroup, config *config.Config,  db *gorm.DB) {
+func SetupInviteGroup(inviteRoute *gin.RouterGroup, config *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
 	inviteRepository := repositories.NewInviteRepository(db)
-	tokenService := utils.NewTokenService(*config)
 	emailService := utils.NewEmailService(*config)
 	inviteUseCase := usecases.NewInviteUseCase(inviteRepository, *config, tokenService, emailService)
 	inviteController := controllers.NewInviteController(inviteUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	inviteRoute.Use(authMiddleware)
 
 	inviteRoute.POST("", inviteController.InviteUser)
 	inviteRoute.POST("/batch", inviteController.InviteUsers)
