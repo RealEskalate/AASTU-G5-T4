14af21b6901be4288b8f963e50919066fb1288e9
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:10:46 +0300

    Wrap country  endpoints with auth middleware

--- Backend/api/routes/country_setup.go
+++ Backend/api/routes/country_setup.go
@@ -4,15 +4,21 @@ import (
 	"A2SVHUB/api/controllers"
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupCountryGroup(api *gin.RouterGroup, db *gorm.DB) {
+func SetupCountryGroup(api *gin.RouterGroup, db *gorm.DB, tokenService *utils.TokenService) {
 	countryRepo := repositories.NewCountryRepository(*db)
 	countryUseCase := usecases.NewCountryUseCase(countryRepo)
 	countryController := controllers.NewCountryController(countryUseCase, db)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
 
 	api.GET("", countryController.GetAllCountries)
 	api.GET("/:id", countryController.GetCountryByID)
