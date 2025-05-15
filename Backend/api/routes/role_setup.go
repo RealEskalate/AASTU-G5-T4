efc7763bd626e4c2c5c20a8fe1c920aa2693e05a
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:12:20 +0300

    Wrap role endpoints with auth middleware

--- Backend/api/routes/role_setup.go
+++ Backend/api/routes/role_setup.go
@@ -4,16 +4,22 @@ import (
 	"A2SVHUB/api/controllers"
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 	"context"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupRoleGroup(roleRoute *gin.RouterGroup, db *gorm.DB) {
+func SetupRoleGroup(roleRoute *gin.RouterGroup, db *gorm.DB, tokenService *utils.TokenService) {
 	roleRepository := repositories.NewRoleRepository(db, context.TODO())
 	roleUseCase := usecases.NewRoleUseCase(roleRepository)
 	roleController := controllers.NewRoleController(roleUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	roleRoute.Use(authMiddleware)
 
 	roleRoute.GET("", roleController.GetAllRoles)
 	roleRoute.GET("/:id", roleController.GetRole)
