5bf0cac7425af7e1ace4a00788faad3c427530aa
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:26:02 +0300

    Add function to verify token rawness

--- Backend/pkg/utils/tokenUtils.go
+++ Backend/pkg/utils/tokenUtils.go
@@ -21,10 +21,10 @@ func NewTokenService(config config.Config) *TokenService {
 func (t *TokenService) GenerateAccessToken(userID int, roleID int) (string, error) {
 	claims := jwt.MapClaims{
 
-		"sub":     userID,                        
-		"role_id": roleID,                     
+		"sub":     userID,
+		"role_id": roleID,
 		"iat":     time.Now().Unix(),
-		"exp":     time.Now().Add(time.Hour * 24 * 10).Unix(),             
+		"exp":     time.Now().Add(time.Hour * 24 * 10).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString([]byte(t.config.JWTSecret))
@@ -81,9 +81,18 @@ func (ts *TokenService) GenerateInviteAccessToken(userID int, roleID int) (strin
 	claims := jwt.MapClaims{
 		"user_id": userID,
 		"role_id": roleID,
-		"exp":     time.Now().Add(time.Hour * 24).Unix(), 
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(ts.config.JWTSecret))
+}
+
+func (t *TokenService) VerifyTokenRaw(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(t.config.JWTSecret), nil
+	})
 }
