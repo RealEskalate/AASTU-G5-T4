5d297846a2892a8660acb4f05b3478e0a79123ac
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:13:37 +0300

    Initialize token service then inject it to route setups

--- Backend/api/routes/routes.go
+++ Backend/api/routes/routes.go
@@ -2,35 +2,37 @@ package routes
 
 import (
 	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetupRoutes(r *gin.RouterGroup, config *config.Config, database *gorm.DB) {
-	
+	tokenService := utils.NewTokenService(*config)
+
 	SetupAuthGroup(r.Group("/auth"), config, database)
 
-	SetupUserGroup(r.Group("/user"), config, database)
-	SetupContestGroup(r.Group("/contest"), config, database)
+	SetupUserGroup(r.Group("/user"), config, database, tokenService)
+	SetupContestGroup(r.Group("/contest"), config, database, tokenService)
 
-	SetupRoleGroup(r.Group("/role"), database)
-	SetupGroupGroup(r.Group("/group"), database)
-	SetupCountryGroup(r.Group("/country"), database)
+	SetupRoleGroup(r.Group("/role"), database, tokenService)
+	SetupGroupGroup(r.Group("/group"), database, tokenService)
+	SetupCountryGroup(r.Group("/country"), database, tokenService)
 
 	superGroupRoute := r.Group("/supergroup")
 
-	SetupSuperGroupRoute(superGroupRoute, config, database)
+	SetupSuperGroupRoute(superGroupRoute, config, database, tokenService)
 	trackGroupRoute := r.Group("/track")
-	SetupTrackGroup(trackGroupRoute, config, database)
+	SetupTrackGroup(trackGroupRoute, config, database, tokenService)
 
-	SetupInviteGroup(r.Group("/invites"), config, database)
-	SetupSessionGroup(r.Group("/session"), database)
-	SetupAttendanceGroup(r.Group("/attendance"), database)
+	SetupInviteGroup(r.Group("/invites"), config, database, tokenService)
+	SetupSessionGroup(r.Group("/session"), database, tokenService)
+	SetupAttendanceGroup(r.Group("/attendance"), database, tokenService)
 
 	ProblemRoute := r.Group("/problem")
-	SetupProblem(ProblemRoute, config, database)
+	SetupProblem(ProblemRoute, config, database, tokenService)
 
-	SetupSubmissionGroup(r.Group("/submission"), config, database)
+	SetupSubmissionGroup(r.Group("/submission"), config, database, tokenService)
 
 }
