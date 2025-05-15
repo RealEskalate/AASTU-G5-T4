8d2eb2c2bc9ca0582220240cd9087cd4c18b17a9
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:14:48 +0300

    Wrap submission endpoints with auth middleware

--- Backend/api/routes/submission_setup.go
+++ Backend/api/routes/submission_setup.go
@@ -1,24 +1,30 @@
 package routes
 
 import (
-    "A2SVHUB/api/controllers"
-    "A2SVHUB/internal/repositories"
-    "A2SVHUB/internal/usecases"
-    "A2SVHUB/pkg/config"
+	"A2SVHUB/api/controllers"
+	"A2SVHUB/internal/repositories"
+	"A2SVHUB/internal/usecases"
+	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func SetupSubmissionGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB) {
-    submissionRepo := repositories.NewSubmissionRepository(db)
-    submissionUseCase := usecases.NewSubmissionUseCase(submissionRepo)
+func SetupSubmissionGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
+	submissionRepo := repositories.NewSubmissionRepository(db)
+	submissionUseCase := usecases.NewSubmissionUseCase(submissionRepo)
 
-    submissionController := controllers.NewSubmissionController(submissionUseCase)
+	submissionController := controllers.NewSubmissionController(submissionUseCase)
 
-    api.POST("", submissionController.CreateSubmission)
-    api.GET("/:id", submissionController.GetSubmissionByID)
-    api.GET("/problem", submissionController.GetSubmissionsByProblem)
-    api.PUT("/:id", submissionController.UpdateSubmission)
-    api.DELETE("/:id", submissionController.DeleteSubmission)
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
+
+	api.POST("", submissionController.CreateSubmission)
+	api.GET("/:id", submissionController.GetSubmissionByID)
+	api.GET("/problem", submissionController.GetSubmissionsByProblem)
+	api.PUT("/:id", submissionController.UpdateSubmission)
+	api.DELETE("/:id", submissionController.DeleteSubmission)
 }
