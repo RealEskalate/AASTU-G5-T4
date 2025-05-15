274c6a42d994574e1a82380878262c723f1f6150
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:15:18 +0300

    Wrap supergroup endpoints with auth middleware

--- Backend/api/routes/superGroup_setup.go
+++ Backend/api/routes/superGroup_setup.go
@@ -5,16 +5,22 @@ import (
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
 	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupSuperGroupRoute(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB) {
+func SetupSuperGroupRoute(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
 	superGroupRepo := repositories.NewSuperSuperGroupRepository(*db)
 	superGroupUseCase := usecases.NewSuperGroupUseCase(superGroupRepo)
 
 	SuperGroupController := controllers.NewSuperGroupController(superGroupUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
 
 	api.GET("", SuperGroupController.GetAllSuperGroups)
 	api.POST("", SuperGroupController.CreateSuperGroup)
