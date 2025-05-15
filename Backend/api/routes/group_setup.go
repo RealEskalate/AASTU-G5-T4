618ea84c5f73c8f98ae3256a46523864b4995a35
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:11:13 +0300

    Wrap GROUP endpoints with auth middleware

--- Backend/api/routes/group_setup.go
+++ Backend/api/routes/group_setup.go
@@ -4,15 +4,21 @@ import (
 	"A2SVHUB/api/controllers"
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
-
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
-func SetupGroupGroup(api *gin.RouterGroup, db *gorm.DB) {
+
+func SetupGroupGroup(api *gin.RouterGroup, db *gorm.DB, tokenService *utils.TokenService) {
 	GroupRepo := repositories.NewGroupRepository(db)
 	GroupUseCase := usecases.NewGroupUseCase(GroupRepo)
 	GroupController := controllers.NewGroupController(GroupUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
 
 	api.GET("", GroupController.GetAllGroups)
 	api.GET("/:id", GroupController.GetGroupByID)
