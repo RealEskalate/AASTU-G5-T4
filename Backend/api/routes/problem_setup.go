10885db59eca143109ea80d0c8f3ca36c27414a1
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:11:57 +0300

    Wrap problem endpoints with auth middleware

--- Backend/api/routes/problem_setup.go
+++ Backend/api/routes/problem_setup.go
@@ -5,15 +5,21 @@ import (
 	"A2SVHUB/internal/repositories"
 	"A2SVHUB/internal/usecases"
 	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func SetupProblem(api *gin.RouterGroup, config *config.Config, db *gorm.DB) {
+func SetupProblem(api *gin.RouterGroup, config *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
 	problemRepository := repositories.NewProblemRepository(*db)
 	problemUseCase := usecases.NewProblemUseCase(problemRepository)
 	problemController := controllers.NewProblemController(problemUseCase)
+
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
 
 	api.POST("", problemController.CreateProblem)
 	api.GET("", problemController.GetProblems)
