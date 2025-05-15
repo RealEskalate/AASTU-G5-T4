1d9a516ab66dc6e84f4dedd94539ccb8773997b5
nahom-zewdu <nahiyo86@gmail.com>
2025-05-15 19:20:54 +0300

    Wrap track endpoints with auth middleware

--- Backend/api/routes/track_setup.go
+++ Backend/api/routes/track_setup.go
@@ -1,25 +1,31 @@
 package routes
 
 import (
-    "A2SVHUB/api/controllers"
-    "A2SVHUB/internal/repositories"
-    "A2SVHUB/internal/usecases"
-    "A2SVHUB/pkg/config"
+	"A2SVHUB/api/controllers"
+	"A2SVHUB/internal/repositories"
+	"A2SVHUB/internal/usecases"
+	"A2SVHUB/pkg/config"
+	"A2SVHUB/pkg/middleware"
+	"A2SVHUB/pkg/utils"
 
-    "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func SetupTrackGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB) {
-    trackRepo := repositories.NewTrackRepository(db)
-    trackUseCase := usecases.NewTrackUseCase(trackRepo)
+func SetupTrackGroup(api *gin.RouterGroup, cfg *config.Config, db *gorm.DB, tokenService *utils.TokenService) {
+	trackRepo := repositories.NewTrackRepository(db)
+	trackUseCase := usecases.NewTrackUseCase(trackRepo)
 
-    trackController := controllers.NewTrackController(trackUseCase)
+	trackController := controllers.NewTrackController(trackUseCase)
 
-    api.GET("", trackController.GetAllTracks)
-    api.POST("", trackController.CreateTrack)
-    api.GET("/:id", trackController.GetTrackByID)
-    api.GET("/search", trackController.GetTracksByName)
-    api.PUT("/:id", trackController.UpdateTrack)
-    api.DELETE("/:id", trackController.DeleteTrack)
+	// Middleware
+	authMiddleware := middleware.AuthMiddleware(tokenService)
+	api.Use(authMiddleware)
+
+	api.GET("", trackController.GetAllTracks)
+	api.POST("", trackController.CreateTrack)
+	api.GET("/:id", trackController.GetTrackByID)
+	api.GET("/search", trackController.GetTracksByName)
+	api.PUT("/:id", trackController.UpdateTrack)
+	api.DELETE("/:id", trackController.DeleteTrack)
 }
